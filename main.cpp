f0c8fdb5bcf869ebd593e3747733baa3dfc234d4
andreastedile <andrea.stedile@studenti.unitn.it>
2022-07-19 13:31:22 +0200

    Init repo

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <argparse/argparse.hpp>
+#include <iostream>
+#include <stdexcept>
+#include <spdlog/spdlog.h>
+#include <stb_image.h>
+
+int main(int argc, char *argv[]) {
+    argparse::ArgumentParser app("Barnes–Hut simulation");
+    app.add_argument("input")
+            .required()
+            .help("specify the input file");
+    app.add_argument("--max-depth")
+            .scan<'d', int>()
+            .default_value(8)
+            .help("specify the max depth");
+    app.add_argument("--detail-threshold")
+            .scan<'d', int>()
+            .default_value(13)
+            .help("specify the detail threshold");
+    app.add_argument("--reduction")
+            .scan<'d', int>()
+            .default_value(0)
+            .help("specify the reduction");
+
+    try {
+        app.parse_args(argc, argv);
+    } catch (const std::runtime_error &err) {
+        std::cerr << app;
+        std::exit(1);
+    }
+
+    auto input = app.get("input");
+    auto max_depth = app.get<int>("--max-depth");
+    auto detail_threshold = app.get<int>("--detail-threshold");
+
+    // https://github.com/gabime/spdlog/wiki/3.-Custom-formatting
+    // %o Elapsed time in milliseconds since previous message
+    // %^ start color range (can be used only once)
+    // %l The log level of the message
+    // %$ end color range (for example %^[+++]%$ %v) (can be used only once)
+    // %v The actual text to log
+    spdlog::set_pattern("[elapsed: %o ms] [%^%l%$] %v");
+
+    spdlog::info("Reading {}...", input);
+
+    int n_cols, n_rows, n;
+    uint8_t* data = stbi_load(input.c_str(), &n_cols, &n_rows, &n, 3);
+    if (!data) {
+        throw std::runtime_error("Could not open file " + input);
+    }
+
+    spdlog::info("Image is {}x{} px", n_rows, n_cols);
+
+    stbi_image_free(data);
+}
