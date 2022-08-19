e70b55e838c2294f825b894d8ac1dbd578d357e9
andreastedile <andrea.stedile@studenti.unitn.it>
2022-08-19 15:30:08 +0200

    19 August: Partial work

--- main.cu
+++ main.cu
@@ -1,13 +1,44 @@
 #include "flatten.h"
 #include "gpu_types.h"
+#include "node.h"
 
 #include <argparse/argparse.hpp>
+#include <cmath>
+#include <cstdlib>
 #include <cuda_runtime.h>
 #include <iostream>
 #include <spdlog/spdlog.h>
 #include <stb_image.h>
 #include <stb_image_write.h>
 #include <stdexcept>
+
+__global__ void init_quadtree_leaves(U8ArraySoa soa, Node *quadtree_nodes, int tree_height, int n_rows, int n_cols) {
+    unsigned int tid = threadIdx.x;
+    auto r = soa.r + blockIdx.x * blockDim.x;
+    auto g = soa.g + blockIdx.x * blockDim.x;
+    auto b = soa.b + blockIdx.x * blockDim.x;
+
+    int offset = static_cast<int>(std::pow(4, tree_height) / 3);
+
+    // Skip internal nodes
+    Node *leaves = quadtree_nodes + offset;
+    // Points to the position in the leaves subarray where this block will start to write
+    leaves = leaves + blockIdx.x * blockDim.x;
+
+    int pos = tid + blockIdx.x * blockDim.x;
+    int i = pos / n_cols;
+    int j = pos % n_cols;
+
+    Pixel color = {r[tid], g[tid], b[tid]};
+    RGB<double> mean = {double(r[tid]), double(g[tid]), double(b[tid])};
+    RGB<double> std = {0.0, 0.0, 0.0};
+
+    leaves[tid] = Node(0, tree_height, i, j, 1, 1, color, mean, std, true);
+}
+
+bool is_power_of_four(int n) {
+    return n == pow(4, (log(n) / log(4)));
+}
 
 int main(int argc, char *argv[]) {
     argparse::ArgumentParser app("jqc");
@@ -33,7 +64,7 @@ int main(int argc, char *argv[]) {
         app.parse_args(argc, argv);
     } catch (const std::runtime_error &err) {
         std::cerr << app;
-        std::exit(1);
+        std::exit(EXIT_FAILURE);
     }
 
     auto input = app.get("input");
@@ -53,29 +84,48 @@ int main(int argc, char *argv[]) {
     int n_cols, n_rows, n;
     uint8_t *pixels = stbi_load(input.c_str(), &n_cols, &n_rows, &n, 3);
     if (!pixels) {
-        throw std::runtime_error("Could not open file " + input);
+        spdlog::error("Could not open file " + input);
+        std::exit(EXIT_FAILURE);
     }
 
     spdlog::info("Image is {}x{} px", n_rows, n_cols);
 
-    spdlog::info("Flattening image...");
-    auto color_soa = flatten(pixels, n_rows * n_cols);
+    if (n_cols != n_rows) {
+        spdlog::error("Image is not square");
+        std::exit(EXIT_FAILURE);
+    }
+    if (!is_power_of_four(n_rows) || !is_power_of_four(n_cols)) {
+        spdlog::error("The number of pixels on the sides of the image is not a power of four");
+        std::exit(EXIT_FAILURE);
+    }
 
+    int n_pixels = n_rows * n_cols;
+
+    spdlog::info("Flattening image...");
+    auto color_soa = flatten(pixels, n_pixels);
+
+    spdlog::info("Copying pixels onto device...");
     U8ArraySoa device_soa;
 
-    cudaMalloc(&device_soa.r, n_rows * n_cols);
-    cudaMalloc(&device_soa.g, n_rows * n_cols);
-    cudaMalloc(&device_soa.b, n_rows * n_cols);
+    cudaMalloc(&device_soa.r, n_pixels * sizeof(uint8_t));
+    cudaMalloc(&device_soa.g, n_pixels * sizeof(uint8_t));
+    cudaMalloc(&device_soa.b, n_pixels * sizeof(uint8_t));
 
-    cudaMemcpy(device_soa.r, color_soa.r.data(), n_rows * n_cols, cudaMemcpyHostToDevice);
-    cudaMemcpy(device_soa.g, color_soa.g.data(), n_rows * n_cols, cudaMemcpyHostToDevice);
-    cudaMemcpy(device_soa.b, color_soa.b.data(), n_rows * n_cols, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_soa.r, color_soa.r.data(), n_pixels * sizeof(uint8_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(device_soa.g, color_soa.g.data(), n_pixels * sizeof(uint8_t), cudaMemcpyHostToDevice);
+    cudaMemcpy(device_soa.b, color_soa.b.data(), n_pixels * sizeof(uint8_t), cudaMemcpyHostToDevice);
 
+    int tree_height = static_cast<int>(std::log(n_pixels) / std::log(4));
+    int n_nodes = static_cast<int>(std::pow(4, tree_height + 1) / 3);
+    Node *quadtree_nodes = NULL;
+    cudaMalloc(&quadtree_nodes, n_nodes * sizeof(Node));
 
-    cudaMemcpy(color_soa.r.data(), device_soa.r, n_rows * n_cols, cudaMemcpyDeviceToHost);
-    cudaMemcpy(color_soa.g.data(), device_soa.g, n_rows * n_cols, cudaMemcpyDeviceToHost);
-    cudaMemcpy(color_soa.b.data(), device_soa.b, n_rows * n_cols, cudaMemcpyDeviceToHost);
-    
+    spdlog::info("Copying pixels back to host...");
+
+    cudaMemcpy(color_soa.r.data(), device_soa.r, n_pixels * sizeof(uint8_t), cudaMemcpyDeviceToHost);
+    cudaMemcpy(color_soa.g.data(), device_soa.g, n_pixels * sizeof(uint8_t), cudaMemcpyDeviceToHost);
+    cudaMemcpy(color_soa.b.data(), device_soa.b, n_pixels * sizeof(uint8_t), cudaMemcpyDeviceToHost);
+
     cudaFree(device_soa.r);
     cudaFree(device_soa.g);
     cudaFree(device_soa.b);
--- node.h
+++ node.h
@@ -3,13 +3,16 @@
 
 #include "pixel.h"
 class Node {
-    const int height;
-    const int depth;
+public:
+    __host__ __device__ Node(const int height, const int depth, const int i, const int j, const int nRows, const int nCols, const Pixel &mColor, const RGB<double> &mMean, const RGB<double> &mStd, bool isLeaf)
+        : height(height), depth(depth), i(i), j(j), n_rows(nRows), n_cols(nCols), m_color(mColor), m_mean(mMean), m_std(mStd), is_leaf(isLeaf) {}
+    int height;
+    int depth;
 
-    const int i;
-    const int j;
-    const int n_rows;
-    const int n_cols;
+    int i;
+    int j;
+    int n_rows;
+    int n_cols;
 
     Pixel m_color;
     RGB<double> m_mean;
